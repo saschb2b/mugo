bf17cb5057281a58082c01f1f92fa16b0e4a9e1c
Sascha Becker <s.becker@wertarbyte.com>
2016-11-07 11:18:57 +0100

    Add variable railCount.

--- mugo/TunnelSegment.cs
+++ mugo/TunnelSegment.cs
@@ -7,7 +7,8 @@ namespace Mugo
 {
     public class TunnelSegment : BaseObject3D
 	{
-		public const int Width = 2;
+		public const float Width = 6;
+        public const float RailCount = 3;
 		public const int Height = 3;
 		public const int Depth = 10;
 
@@ -45,16 +46,20 @@ namespace Mugo
 				Vector3.One, Vector3.One, Vector3.One,
 				new Vector2(0.5f, 0.4f), new Vector2(0.5f, 0.7f), new Vector2(0f, 0.7f));
 
-			//bottom
-			addTriangle(
-				new Vector3(Width, 0, 0), new Vector3(0, 0, -Depth), new Vector3(0, 0, 0), 
-				Vector3.One, Vector3.One, Vector3.One,
-				new Vector2(0.3f, 0.4f), new Vector2(0, 0), new Vector2(0, 0.4f));
+            //bottom
+            for (float i = 0; i < RailCount; i++)
+            {
+                addTriangle(
+                new Vector3(Width / RailCount + (Width / RailCount * i), 0, 0), new Vector3(Width / RailCount * i, 0, -Depth), new Vector3(Width / RailCount * i, 0, 0),
+                Vector3.One, Vector3.One, Vector3.One,
+                new Vector2(0.3f, 0.4f), new Vector2(0, 0), new Vector2(0, 0.4f));
 
-			addTriangle(
-				new Vector3(Width, 0, 0), new Vector3(Width, 0, -Depth), new Vector3(0, 0, -Depth), 
-				Vector3.One, Vector3.One, Vector3.One,
-				new Vector2(0.3f, 0.4f),new Vector2(0.3f, 0f), new Vector2(0f, 0f));
+                addTriangle(
+                    new Vector3(Width / RailCount + (Width / RailCount * i), 0, 0), new Vector3(Width / RailCount + (Width / RailCount * i), 0, -Depth), new Vector3(Width / RailCount * i, 0, -Depth),
+                    Vector3.One, Vector3.One, Vector3.One,
+                    new Vector2(0.3f, 0.4f), new Vector2(0.3f, 0f), new Vector2(0f, 0f));
+            }
+			
 
 			//top
 			addTriangle(

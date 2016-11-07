4aea5ddd4d1cc95fd966edf2dc5506edcd082c16
DarkDrek <wackerei@gmail.com>
2016-11-07 09:42:58 +0100

    Rebase with master.

--- mugo/Tunnel.cs
+++ mugo/Tunnel.cs
@@ -3,6 +3,7 @@ using Engine.cgimin.material.simpletexture;
 using Engine.cgimin.material;
 using OpenTK;
 using System.Collections.Generic;
+using OpenTK.Graphics.ES10;
 
 namespace Mugo
 {
@@ -12,21 +13,44 @@ namespace Mugo
 		private readonly SimpleTextureMaterial material;
 
 		public Tunnel()
-		{
-			segments = new List<TunnelSegment>() {
-				new TunnelSegment(),
-				new TunnelSegment(),
-				new TunnelSegment()
-			};
+        {
+            segments = new List<TunnelSegment>() {
+                new TunnelSegment(),
+                new TunnelSegment(),
+                new TunnelSegment(),
+                new TunnelSegment(),
+                new TunnelSegment(),
+                new TunnelSegment(),
+                new TunnelSegment(),
+                new TunnelSegment(),
+            };
 
-			material = new SimpleTextureMaterial();
+            material = new SimpleTextureMaterial();
 
-			for (int i = 0; i < segments.Count; i++) {
-				segments[i].Transformation *= Matrix4.CreateTranslation(0, 0, (i - 1) * -TunnelSegment.Depth);
-			}
-		}
+            CalculateTransformations();
+        }
 
-		public void Draw()
+        private void CalculateTransformations()
+        {
+            for (var i = 0; i < segments.Count; i++)
+            {
+                segments[i].Transformation = Matrix4.CreateTranslation(0, 0, (i - 2) * -TunnelSegment.Depth);
+            }
+        }
+
+	    public void GenerateNextSegment(Action<TunnelSegment> segementModifier)
+	    {
+            segments[0].UnLoad();
+            segments.RemoveAt(0);
+
+            var newSegment = new TunnelSegment();
+            segementModifier?.Invoke(newSegment);
+            segments.Add(newSegment);
+
+            CalculateTransformations();
+	    }
+
+        public void Draw()
 		{
 			foreach(var segment in segments) {
 				material.Draw(segment, segment.TextureId);

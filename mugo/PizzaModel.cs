64cd84c1160684c88a18d7d80c8268ff1d53c65e
DarkDrek <wackerei@gmail.com>
2016-11-18 16:54:37 +0100

    Cache pizza model's vao object.

--- mugo/PizzaModel.cs
+++ mugo/PizzaModel.cs
@@ -6,41 +6,50 @@ using OpenTK;
 
 namespace Mugo
 {
-    class PizzaModel : ObjLoaderObject3D, ITunnelSegementElementModel
-    {
-        private const String objFilePath = "data/objects/Pizza.model";
-        private const String texturePath = "data/textures/Pizza.png";
-        private static readonly SimpleTextureMaterial material = new SimpleTextureMaterial();
+	class PizzaModel : BaseObject3D, ITunnelSegementElementModel
+	{
+		private static readonly PizzaModelInternal internalModel = new PizzaModelInternal();
+		private static readonly SimpleTextureMaterial material = new SimpleTextureMaterial();
 
-        public PizzaModel() : base(objFilePath)
-        {
-            TextureId = TextureManager.LoadTexture(texturePath);
-            Transformation *= Matrix4.CreateScale(0.2f);
-            Transformation *= Matrix4.CreateRotationY(MathHelper.DegreesToRadians(90));
-            Transformation *= Matrix4.CreateRotationX(MathHelper.DegreesToRadians(90));            
-            Transformation *= Matrix4.CreateTranslation(0f, 1.5f, -TunnelSegment.Depth / 2f);
+		public Matrix4 DefaultTransformation { get; private set;}
 
-			DefaultTransformation = Transformation;
-        }
-
-        public int TextureId { get; private set; }
-
-        public Matrix4 DefaultTransformation { get; }
-
-		public void ResetTransformation()
-		{
-			Transformation = DefaultTransformation;
+		public new Matrix4 Transformation {
+			get { return base.Transformation; }
+			set { base.Transformation = value; }
 		}
 
-        public new Matrix4 Transformation
-        {
-            get { return base.Transformation; }
-            set { base.Transformation = value; }
-        }
+		public PizzaModel ()
+		{
+			DefaultTransformation = internalModel.DefaultTransformation;
+			Transformation = internalModel.Transformation;
+			Vao = internalModel.Vao;
+			Indices = internalModel.Indices;
+		}
 
-        public void Draw()
-        {
-            material.Draw(this, TextureId);
-        }
-    }
+		public void Draw ()
+		{
+			material.Draw(this, internalModel.TextureId);
+		}
+
+		private class PizzaModelInternal : ObjLoaderObject3D
+		{
+			private const String objFilePath = "data/objects/Pizza.model";
+			private const String texturePath = "data/textures/Pizza.png";
+
+			public PizzaModelInternal () : base (objFilePath)
+			{
+				TextureId = TextureManager.LoadTexture (texturePath);
+				Transformation *= Matrix4.CreateScale (0.2f);
+				Transformation *= Matrix4.CreateRotationY (MathHelper.DegreesToRadians (90));
+				Transformation *= Matrix4.CreateRotationX (MathHelper.DegreesToRadians (90));
+				Transformation *= Matrix4.CreateTranslation (0f, 1.5f, -TunnelSegment.Depth / 2f);
+
+				DefaultTransformation = Transformation;
+			}
+
+			public int TextureId { get; private set; }
+
+			public Matrix4 DefaultTransformation { get; }
+		}
+	}
 }
